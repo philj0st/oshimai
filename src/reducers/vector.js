4fc83655bcc53535c973c027eb8d12e940c26cc0
Philipp Jost <philx_x@hotmail.com>
2016-05-29 19:13:02 -0400

    add vector reducer utility function and tests

--- src/reducers/vector.js
+++ src/reducers/vector.js
@@ -8,16 +8,22 @@ const degrees = 180 / Math.PI
 const radian2degrees = rad => rad * degrees
 const degrees2radian = deg => deg / degrees
 
+// what a neat ES6 usage :)
+const vectorLength = ({ x, y }) => Math.sqrt(x * x + y * y)
+
 // actual reducer
 const vector = (vector = {x:0, y:0}, action) => {
   switch (action.type) {
     case 'VECTOR_ADD':
+    // TODO: should i destructure x and y from vector for all cases?
       return {
         x: vector.x + action.vector.x,
         y: vector.y + action.vector.y
       }
 
     // http://victorjs.org/#rotate
+    // Rotates the vector to a certain angle, in radians CCW from +X axis.
+    // @param action.angle - angle in radians
     case 'VECTOR_ROTATE':
       // create variables for x and y of the old state
       let { x: xOld, y: yOld } = vector
@@ -34,4 +40,4 @@ const vector = (vector = {x:0, y:0}, action) => {
       return vector
   }
 }
- module.exports = vector
+module.exports = vector
--- test/vector.js
+++ test/vector.js
@@ -6,4 +6,16 @@ expect(
   vector({x: 20, y:30}, {type: 'VECTOR_ADD', vector: {x: 30, y:20}})
 ).toEqual({x: 50, y:50})
 
+// make rotations in both directions
+// TODO: fails .. adding PI/2 CW to [0/1] should result in [1/0]
+expect(
+  vector(vector({x:0, y:1}, {type:'VECTOR_ROTATE', angle: -Math.PI/4}), {type:'VECTOR_ROTATE', angle: -Math.PI/4})
+).toEqual(vector({x:1, y:0}, {type:'VECTOR_ROTATE', angle: Math.PI/4}))
+
+// persist length across rotations
+const vectorLength = ({ x, y }) => Math.sqrt(x * x + y * y)
+expect(
+  vectorLength(vector({x:1, y:0}, {type:'VECTOR_ROTATE', angle: Math.PI/4}))
+).toEqual(1)
+
 console.log('vector reducer tests passed');
